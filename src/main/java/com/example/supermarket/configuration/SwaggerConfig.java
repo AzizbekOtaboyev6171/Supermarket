3d81658e7ea9d92f9fb2422a6f7b32d6b698483a
OtaboyevAzizbek <azizbekotaboyev7735@gmail.com>
2024-09-02 18:37:25 +0500

    Have some changes.

--- src/main/java/com/example/supermarket/configuration/SwaggerConfig.java
+++ src/main/java/com/example/supermarket/configuration/SwaggerConfig.java
@@ -2,14 +2,18 @@ package com.example.supermarket.configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
+        Server server = new Server().url("https://3.64.194.123:8080").description("Production Server");
         return new OpenAPI()
                 .info(new Info().title("Supermarket API")
                         .description("This is a sample Spring Boot RESTful service leveraging springdoc-openapi with OpenAPI 3 for API documentation")
@@ -20,6 +24,7 @@ public class SwaggerConfig {
                 .addTagsItem(new Tag().name("Category").description("Methods for Category"))
                 .addTagsItem(new Tag().name("Product").description("Methods for Product"))
                 .addTagsItem(new Tag().name("Product Price History").description("Methods for Product Price History"))
-                .addTagsItem(new Tag().name("Unit Template").description("Methods for Unit Template"));
+                .addTagsItem(new Tag().name("Unit Template").description("Methods for Unit Template"))
+                .servers(List.of(server));
     }
 }
